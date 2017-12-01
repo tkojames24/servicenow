4e905ba8d110f908038dc2a2205617001f122305
tkojames <tkojames@gmail.com>
2017-12-01 13:33:39 -0800

    Added script for Record Prouducer for onboarding.

---
+++ Onboarding.js
@@ -0,0 +1,19 @@
+var sdt =  producer.afterhours.getGlideObject().getQuestion().getLabel()  + " " + producer.sdt + "\n";
+// getglideobject is var holding the user input info
+var username = producer.dbascript.getGlideObject().getQuestion().getLabel() + " " + producer.username + "\n";
+var computer = producer.env.getGlideObject().getQuestion().getLabel() + " " + producer.computer + "\n";
+var poc = producer.u_application.getGlideObject().getQuestion().getLabel()  + " " + producer.poc + "\n";
+var request = producer.addcomment.getGlideObject().getQuestion().getLabel()  + " " + producer.request + "\n";
+var Team = producer.dbplatform.getGlideObject().getQuestion().getLabel()  + " " + producer.team + "\n";
+var staff = producer.dbplatform.getGlideObject().getQuestion().getLabel()  + " " + producer.staff + "\n";
+
+
+//place all var that need to be in body into the array
+var array;
+array = [afterhours, dba, env, app, comment, dbaenv];
+
+
+
+
+current.setValue('u_original_body', array.join(','));
+
