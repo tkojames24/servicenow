abb28c93a76e4d13492a7d7f08ba53ed3327a175
tkojames <tkojames@gmail.com>
2017-12-05 14:04:41 -0800

    Fixed onboarding script that had wrong var names.

--- Onboarding.js
+++ Onboarding.js
@@ -1,11 +1,11 @@
-var sdt =  producer.afterhours.getGlideObject().getQuestion().getLabel()  + " " + producer.sdt + "\n";
+var sdt =  producer.sdt.getGlideObject().getQuestion().getLabel()  + " " + producer.sdt + "\n";
 // getglideobject is var holding the user input info
-var username = producer.dbascript.getGlideObject().getQuestion().getLabel() + " " + producer.username + "\n";
-var computer = producer.env.getGlideObject().getQuestion().getLabel() + " " + producer.computer + "\n";
-var poc = producer.u_application.getGlideObject().getQuestion().getLabel()  + " " + producer.poc + "\n";
-var request = producer.addcomment.getGlideObject().getQuestion().getLabel()  + " " + producer.request + "\n";
-var Team = producer.dbplatform.getGlideObject().getQuestion().getLabel()  + " " + producer.team + "\n";
-var staff = producer.dbplatform.getGlideObject().getQuestion().getLabel()  + " " + producer.staff + "\n";
+var username = producer.username.getGlideObject().getQuestion().getLabel() + " " + producer.username + "\n";
+var computer = producer.computer.getGlideObject().getQuestion().getLabel() + " " + producer.computer + "\n";
+var poc = producer.poc.getGlideObject().getQuestion().getLabel()  + " " + producer.poc + "\n";
+var request = producer.request.getGlideObject().getQuestion().getLabel()  + " " + producer.request + "\n";
+var Team = producer.Team.getGlideObject().getQuestion().getLabel()  + " " + producer.team + "\n";
+var staff = producer.staff.getGlideObject().getQuestion().getLabel()  + " " + producer.staff + "\n";
 
 
 //place all var that need to be in body into the array
@@ -13,7 +13,4 @@ var array;
 array = [sdt,username,computer,poc,request,Team,staff];
 
 
-
-
 current.setValue('u_original_body', array.join(','));
-
