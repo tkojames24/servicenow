77315aaa11155ef62e0a78e68889511f60534689
tkojames <tkojames@gmail.com>
2018-02-09 10:13:06 -0800

    added new var to deploy record producer requested by Adam.

---
+++ jira 2.js
@@ -0,0 +1,12 @@
+
+var type = send-ready;
+
+
+var notification_type = SMTP;
+
+
+var subject = "Please grant the following users access to Jira and Confleunce with ITS-Write";
+
+var recipients  = "ejmathison@ucdavis.edu";
+
+var body = "Please grant the following user" + jira;
